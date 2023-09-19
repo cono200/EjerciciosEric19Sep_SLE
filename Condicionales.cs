6020a601c63c6ec5f20c94cd36b46cf2b34df3a5
Thomas Orozco <thomasorozcog@gmail.com>
2023-09-19 08:22:24 -0700

    Agregar archivos de proyecto.

---
+++ Condicionales.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sadadsdadd121
+{
+    internal class Condicionales
+    {
+        public string ParImpar(int numero)
+        {
+            if (numero > 0)
+            {
+                return "El numero es positivo";
+            }
+            else if (numero > 0)
+            {
+                return "El numero es negativo";
+            }
+            return "Es igual a cero";
+
+        }
+
+
+        public int MayorMenor(int numero1, int numero2, int numero3)
+        {
+            if (numero1 > numero2 && numero1 > numero3)
+            {
+                Console.WriteLine(numero1);
+            }
+            else if (numero2 > numero1 && numero2 > numero3)
+            {
+                return numero2;
+            }
+            else if (numero3 > numero1 && numero3 > numero1)
+            {
+                return numero3;
+            }
+
+            return numero1;
+        }
+
+
+        public int Calificaciones(int numero)
+        {
+            if (numero >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (numero < 90 && numero >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (numero < 80 && numero >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (numero < 70 && numero >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("E");
+            }
+            return numero;
+        }
+
+
+        public int NumAleatorio(int numero)
+        {
+
+        }
+
+
+
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace sadadsdadd121
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Condicionales condicionales1 = new Condicionales();
+            /*
+             EJERCICIO DE SABER SI EL NUMERO ES PAR O IMPAR
+            Console.WriteLine("Ingresa el numero");
+            int numero= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(condicionales1.ParImpar(numero));
+            
+            
+
+
+          EJERCICIO DE QUIEN DE LOS 3 NUMERO ES MAYOR
+            Console.WriteLine("Ingresa el primer numero");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el tercero");
+            int numero3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(condicionales1.MayorMenor(numero1,numero2,numero3));
+
+            */
+
+            /*
+             * EJERCICIO DE LAS CALIFICACIONES
+            Console.WriteLine("Ingresa la calificacion");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(condicionales1.Calificaciones(numero));
+            */
+        }
+    }
+}
